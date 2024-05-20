cf2b9f3972b7c69704427aa65c953f6076048094
Christopher Montalban <chris.montalban@gmail.com>
2024-05-19 20:53:03 -0400

    Unversion files

---
+++ soardr/ghts/recipes/sq/recipes_BIAS.py
@@ -0,0 +1,30 @@
+"""
+Recipes available to data with tags ['GMOS', 'CAL', 'BIAS'].
+Default is "makeProcessedBias".
+"""
+recipe_tags = {'GHTS', 'CAL', 'BIAS'}
+
+def makeProcessedBias(p):
+    """
+     This recipe performs the standardization and corrections needed to convert
+     the raw input bias images into a single stacked bias image. This output
+     processed bias is stored on disk using storeProcessedBias and has a name
+     equal to the name of the first input bias image with "_bias.fits" appended.
+
+     Parameters
+     ----------
+     p : PrimitivesBASE object
+         A primitive set matching the recipe_tags.
+     """
+
+    # p.prepare()
+    # p.addDQ(add_illum_mask=False)
+    # p.addVAR(read_noise=True)
+    #p.overscanCorrect()
+    # p.stackBiases()
+    # p.storeProcessedBias()
+    p.helloWorld()
+    return
+
+
+_default = makeProcessedBias
